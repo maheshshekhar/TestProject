3403ccd4e50c160d07b4876cfce47437d2a0cb74
maheshshekhar <maheshshekhar@192.168.1.3>
2014-12-12 22:06:29 +0530

    First Commit

---
+++ TestProject/src/com/mahesh/test/TestProject.java
@@ -0,0 +1,11 @@
+package com.mahesh.test;
+
+public class TestProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is a test");
+	}
+
+}
